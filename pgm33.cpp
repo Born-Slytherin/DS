ba682d4eb4b63d51e3dcf3fd3c284de34fb20957
Slytherin⚕️ <142644436+Born-Slytherin@users.noreply.github.com>
2023-12-16 09:29:31 +0530

    Update

---
+++ pgm33.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *start;
+    int n = 0;
+
+public:
+    LinkedList()
+    {
+        start = nullptr;
+    }
+
+    void DispLL()
+    {
+        Node *node = start;
+        cout << "\nList: ";
+        while (node != nullptr)
+        {
+            cout << node->data << " --> ";
+            node = node->next;
+        }
+        cout << "NULL" << endl
+             << endl;
+    }
+
+    void CreateLL()
+    {
+        int x;
+        n++;
+        cout << endl
+             << "Enter element " << n << " : ";
+        cin >> x;
+        Insert(x);
+
+        char ch;
+        cout << "Enter 'y' to continue : ";
+        cin >> ch;
+
+        if (ch == 'y')
+        {
+            CreateLL();
+        }
+    }
+
+    void Insert(int x)
+    {
+        Node *temp = new Node;
+        temp->data = x;
+        temp->next = nullptr;
+
+        if (start == nullptr)
+        {
+            start = temp;
+        }
+        else
+        {
+            Node *curr = start;
+            while (curr->next != nullptr)
+            {
+                curr = curr->next;
+            }
+            curr->next = temp;
+        }
+    }
+
+    Node *SearchNode(int pos)
+    {
+        Node *curr = start;
+        for (int i = 1; i < pos; i++)
+        {
+            curr = curr->next;
+        }
+        return curr;
+    }
+
+    void Interchange()
+    {
+        int x, y;
+
+        cout << "Enter two positions to interchange: ";
+        cin >> x >> y;
+
+        if (x > n || y > n || x < 1 || y < 1)
+        {
+            cout << "\n\nInvalid positions\n\n";
+            Interchange();
+            return;
+        }
+
+        if (x == y)
+        {
+            cout << "\n\nPositions are the same; no interchange needed\n\n";
+            return;
+        }
+
+        Node *prevX = NULL, *currX = start;
+        for (int i = 1; i < x && currX != NULL; i++)
+        {
+            prevX = currX;
+            currX = currX->next;
+        }
+
+        Node *prevY = NULL, *currY = start;
+        for (int i = 1; i < y && currY != NULL; i++)
+        {
+            prevY = currY;
+            currY = currY->next;
+        }
+
+        if (currX == NULL || currY == NULL)
+        {
+            cout << "\n\nNode not found, no swap done\n\n";
+            return;
+        }
+
+        if (prevX != NULL)
+        {
+            prevX->next = currY;
+        }
+        else
+        {
+            start = currY;
+        }
+
+        if (prevY != NULL)
+        {
+            prevY->next = currX;
+        }
+        else
+        {
+            start = currX;
+        }
+
+        Node *temp = currX->next;
+        currX->next = currY->next;
+        currY->next = temp;
+
+        cout << "\n\nNodes swapped\n\n";
+        DispLL();
+    }
+};
+
+int main()
+{
+    LinkedList list;
+    list.CreateLL();
+    list.DispLL();
+    list.Interchange();
+    return 0;
+}
