c90c8813097cb48ffe2583e945b476b0fc875405
Slytherin⚕️ <142644436+Born-Slytherin@users.noreply.github.com>
2023-12-16 08:28:28 +0530

    Update

---
+++ pgm30.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *start;
+    int count;
+
+public:
+    LinkedList()
+    {
+        start = nullptr;
+        count = 0;
+    }
+
+    void insert()
+    {
+        int value;
+        int position;
+
+        cout << "Enter the value to insert: ";
+        cin >> value;
+
+        Node *newNode = new Node;
+        newNode->data = value;
+        newNode->next = nullptr;
+
+        if (start == nullptr)
+        {
+            start = newNode;
+        }
+        else
+        {
+            if (count == 0)
+            {
+                cout << "Enter the position to insert: ";
+                cin >> position;
+
+                if (position < 1 || position > count + 1)
+                {
+                    cout << "Invalid position. Inserting at the end." << endl;
+                    position = count + 1;
+                }
+            }
+            else
+            {
+                cout << "Enter the position to insert: ";
+                cin >> position;
+            }
+
+            if (position == 1)
+            {
+                newNode->next = start;
+                start = newNode;
+            }
+            else
+            {
+                Node *temp = start;
+                for (int i = 1; i < position - 1; ++i)
+                {
+                    temp = temp->next;
+                }
+                newNode->next = temp->next;
+                temp->next = newNode;
+            }
+        }
+        count++;
+    }
+
+    void display()
+    {
+        Node *temp = start;
+        if (temp == nullptr)
+        {
+            cout << "\n\nEmpty List!" << endl;
+        }
+        else
+        {
+            cout << "\n\nList: ";
+            while (temp != nullptr)
+            {
+                cout << temp->data << " -> ";
+                temp = temp->next;
+            }
+            cout << "NULL" << endl;
+        }
+    }
+
+    Node *SearchNode(int x)
+    {
+        Node *temp = start;
+        Node *prev = nullptr;
+
+        while (temp != nullptr)
+        {
+            if (temp->data == x)
+            {
+                return prev;
+            }
+            prev = temp;
+            temp = temp->next;
+        }
+
+        cout << "\n\nElement not found!..Re-Try..!" << endl;
+        return nullptr;
+    }
+
+    void deleteNode(int value)
+    {
+
+        if (start == nullptr)
+        {
+            cout << "\n\nEmpty List!" << endl;
+            return;
+        }
+
+        Node *preNode = SearchNode(value);
+
+        if (preNode == nullptr && start->data == value)
+        {
+
+            Node *del = start;
+            start = start->next;
+            cout << "\n\nNode with element " << del->data << " has been deleted successfully!" << endl;
+            delete del;
+        }
+        else if (preNode != nullptr)
+        {
+            Node *del = preNode->next;
+            preNode->next = del->next;
+            cout << "\n\nNode with element " << del->data << " has been deleted successfully!" << endl;
+            delete del;
+        }
+        display();
+    }
+};
+
+int main()
+{
+    LinkedList list;
+    int choice, value;
+
+    while (true)
+    {
+        cout << "\n\n1. Insert into list\n"
+             << "2. Delete from list\n"
+             << "0. Exit\n"
+             << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            char ch;
+            list.insert();
+            while (true)
+            {
+
+                cout << "\n\nDo you want to insert another element : ";
+                cin >> ch;
+
+                if (ch == 'y')
+                {
+                    list.insert();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            list.display();
+            break;
+        case 2:
+            cout << "Enter the value to delete: ";
+            cin >> value;
+            list.deleteNode(value);
+            break;
+        case 0:
+            cout << "\n\nExiting..." << endl;
+            exit(0);
+        default:
+            cout << "Invalid choice. Try again." << endl;
+        }
+    }
+
+    return 0;
+}
